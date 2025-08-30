3768fa148dfb825ddfeb9e0ad1917658777ea8f6
yohannes <yohannegeta9@gmail.com>
2025-08-29 19:49:37 -0600

    check the cors error

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,18 +1,17 @@
-const allowedOrigins =require('./allowedOrigin').default.default
+const allowedOrigins = [
+  'https://jobportalfrontend-j2dg.onrender.com' // remove trailing slash
+];
 
-const corsOption={
-    origin:(origin,callback)=>{
-        if(allowedOrigins.indexOf(origin)!==-1|| !origin){
-            callback(null,true)
-        }
-        else{
-            callback(new Error('Not allowed by cors'))
-
-        }
-        },
-        credentials: true,
-        optionsSuccessStatus:200
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
-module.exports=corsOption
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+}
 
-
+module.exports = corsOptions
